aad03a06486a400494f0432e46e07a535d3f187c
Aditya Pratap Singh <47314947+aditya102@users.noreply.github.com>
2019-02-08 13:44:35 +0530

    Add automated testing and fix bug in question date.  (#4)
    * Add Auto testcase
    
    * Update code with flake8 guideline
    
    * Add unit tests and fix a bug in question date
    
    * Add a line above assertQuery and Update Admin page
    
    - Updated Name of Admin Page
    - Added New Search feature And organize questions
    - Added choice field in default

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -11,8 +12,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -37,4 +37,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
